1fffde92e2d26d2cebb79dc1b2e0efd2ffcbf243
Zain Ul Abideen <127882169+zainulabideen041@users.noreply.github.com>
2024-12-02 19:13:09 +0500

    loader added

--- frontend/src/pages/authors.jsx
+++ frontend/src/pages/authors.jsx
@@ -3,8 +3,12 @@ import AuthorPlaceholder from "../assets/author.jpg";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 import "./style.css";
+import { tailChase } from "ldrs";
+
+tailChase.register();
 
 const Authors = () => {
+  const [loading, setLoading] = useState(true);
   const [authors, setAuthors] = useState([]);
   const navigate = useNavigate();
 
@@ -15,6 +19,7 @@ const Authors = () => {
           "https://backend-umber-chi-47.vercel.app/blog/authors"
         );
         setAuthors(response.data.Authors || []);
+        setLoading(false);
       } catch (error) {
         console.error("Error fetching authors:", error);
       }
@@ -28,40 +33,56 @@ const Authors = () => {
   };
 
   return (
-    <main className="authors-page">
-      <div className="blog-cards">
-        {authors.length === 0 ? (
-          <h2
-            style={{
-              minHeight: "50vh",
-              justifyContent: "center",
-              display: "flex",
-              alignItems: "center",
-            }}
-          >
-            No Authors found
-          </h2>
-        ) : (
-          authors.map((author) => (
-            <div data-aos="fade-up" className="card" key={author._id}>
-              <img
-                src={author.profileImage || AuthorPlaceholder}
-                alt={author.username || "Author"}
-              />
-              <div className="card-content author-card-content">
-                <h3>{author.username}</h3>
-                <span className="card-content-date">
-                  Total Blogs: {author.totalBlogs}
-                </span>
-                <button onClick={() => ViewAuthorBlogs(author._id)}>
-                  View Blogs
-                </button>
-              </div>
-            </div>
-          ))
-        )}
-      </div>
-    </main>
+    <>
+      {loading ? (
+        <div
+          className="loader"
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "60vh",
+          }}
+        >
+          <l-tail-chase size="40" speed="1.75" color="black"></l-tail-chase>
+        </div>
+      ) : (
+        <main className="authors-page">
+          <div className="blog-cards">
+            {authors.length === 0 ? (
+              <h2
+                style={{
+                  minHeight: "50vh",
+                  justifyContent: "center",
+                  display: "flex",
+                  alignItems: "center",
+                }}
+              >
+                No Authors found
+              </h2>
+            ) : (
+              authors.map((author) => (
+                <div data-aos="fade-up" className="card" key={author._id}>
+                  <img
+                    src={author.profileImage || AuthorPlaceholder}
+                    alt={author.username || "Author"}
+                  />
+                  <div className="card-content author-card-content">
+                    <h3>{author.username}</h3>
+                    <span className="card-content-date">
+                      Total Blogs: {author.totalBlogs}
+                    </span>
+                    <button onClick={() => ViewAuthorBlogs(author._id)}>
+                      View Blogs
+                    </button>
+                  </div>
+                </div>
+              ))
+            )}
+          </div>
+        </main>
+      )}
+    </>
   );
 };
 
